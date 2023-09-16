4635248890343a3ff6a79ebbd88d207570bf6d7b
elo6th <126991910+elo6th@users.noreply.github.com>
2023-09-17 01:05:06 +0200

    Create Array.swift

---
+++ Array.swift
@@ -0,0 +1,21 @@
+//
+//  Array.swift
+//  L5Challenge
+//
+//  Created by Ozan Kocakaya on 13.09.23.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+struct tryhard:Identifiable {
+    var gag0:String
+    var id: UUID = UUID()
+    func gaga() {
+        // Zufälligen Wert aus Array in var packen
+        var random:String
+        
+        }
+}
+
